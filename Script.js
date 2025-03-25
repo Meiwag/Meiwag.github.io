8ef667ab006d83963669dbf5e1915ae258d7dbce
Meiwag <felixus7777@icloud.com>
2025-03-25 17:38:09 +0100

    ea

--- Script.js
+++ Script.js
@@ -1,5 +1,20 @@
+function CreateListItem(Name) {
+    const ListItem = document.createElement("li")
+    const Link = document.createElement("a")
+    const TextElement = document.createElement("p")
+
+    TextElement.textContent = PartName
+    Link.textContent = PartName
+    Link.href = "Parts/" + PartName
+    Link.target = "_blank"
+
+    PartList.appendChild(ListItem)
+    ListItem.appendChild(Link)
+    Link:appendChild(TextElement)
+}
+
 function AddPartsToList(PartJson) {
-    const PartList = document.getElementById("part-list")
+    var PartList = document.getElementById("part-list")
 
     let Sorted = []
     for (const Key in PartJson) {
@@ -10,8 +25,10 @@ function AddPartsToList(PartJson) {
     Sorted.forEach(PartName => {
         const Element = document.createElement("li")
         const Link = document.createElement("a")
-        Link.textContent = PartName
-        Link.href = "Parts/"+PartName 
+        const TextElement = document.createElement("p")
+
+        TextElement.textContent = PartName
+        Link.href = "Parts/" + PartName
         Link.target = "_blank"
         PartList.appendChild(Element)
         Element.appendChild(Link)
