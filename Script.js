52034f5cdec445fb15e16f962d86487be49c326f
Meiwag <felixus7777@icloud.com>
2025-03-25 12:04:12 +0100

    scripting

--- Script.js
+++ Script.js
@@ -1,22 +1,24 @@
-const PartList = document.getElementById("part-list")
-let PartJson
+function AddPartsToList(params) {
+    const PartList = document.getElementById("part-list")
+    let PartJson
 
-fetch("Parts.json")
-.then(response => response.json()) // Parse the response
-.then(data => PartJson = data)
-.catch(error => console.error("Error fetching JSON: ", error));
+    fetch("Parts.json")
+        .then(response => response.json()) // Parse the response
+        .then(data => PartJson = data)
+        .catch(error => console.error("Error fetching JSON: ", error));
 
-let Sorted = []
-for (const Key in PartJson) {
-    Sorted[Sorted.length] = Key
+    let Sorted = []
+    for (const Key in PartJson) {
+        Sorted[Sorted.length] = Key
+    }
+    Sorted.sort()
+
+    Sorted.forEach(PartName => {
+        const Element = document.createElement("li")
+
+        Element.textContent = PartName
+        PartList.appendChild(Element)
+
+        console.log("Added " + PartName + " to partlist")
+    });
 }
-Sorted.sort()
-
-Sorted.forEach( PartName => {
-    const Element = document.createElement("li")
-
-    Element.textContent = PartName
-    PartList.appendChild(Element)
-
-    console.log("Added " + PartName + " to partlist")
-});
